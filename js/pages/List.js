79e62524b9efcfb16209207dea5b5c9c8d5e1efe
nugbit123-bit <nugbit123@gmail.com>
2026-04-19 12:12:16 -0400

    grh

--- js/pages/List.js
+++ js/pages/List.js
@@ -103,26 +103,29 @@ export default {
                         Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        No 2 player levels on the list
                     </p>
                     <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Levels can not have a part over 8 cps
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Do not use secret ways
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Have gameplay that is original and NOT copied from elsewhere``
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Levels must be 5 seconds, no more and no less
                     </p>
+                    <p>
+                    Levels must be made for the list
+                    <p>
                 </div>
             </div>
         </main>
